01aded5c3f67b2c9c8769e945361e9a7fcb1f209
Sam <sam@basx.dev>
2021-05-29 16:14:47 +0700

    version bump

--- htmlgenerator/__init__.py
+++ htmlgenerator/__init__.py
@@ -3,5 +3,5 @@ from .htmltags import *  # noqa
 from .lazy import *  # noqa
 from .safestring import mark_safe  # noqa
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __DEBUG__ = False  # not sure whether this is a smart idea, but environment variables do not really seem better...
